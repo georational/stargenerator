5aaa75d31777a54e8ccf006f36b3248a59901840
Andreas <georationalvr@gmail.com>
2018-05-26 23:50:17 +0100

    Additional error handling
    Plus timer and screenshot generation

--- loadspeed.js
+++ loadspeed.js
@@ -1,6 +1,19 @@
 var page = require('webpage').create(),
   system = require('system'),
-  t, address;
+  t, address,
+  hour;
+
+hour = new Date().getHours();
+// console.log(hour);
+
+// TIMEOUT
+page.settings.resourceTimeout = 6666;
+page.onResourceTimeout = function (e) {
+  phantom.exit(1);
+};
+
+if (hour > 0 && hour < 7) // Timer, to run between 0.01am and 6.59am
+{
 
 if (system.args.length === 1) {
   console.log('Usage: loadspeed.js <some URL>');
@@ -9,13 +22,29 @@ if (system.args.length === 1) {
 
 t = Date.now();
 address = system.args[1];
-page.open(address, function(status) {
-  if (status !== 'success') {
-    console.log('FAIL to load the address');
-  } else {
-    t = Date.now() - t;
-    console.log('Loading ' + system.args[1]);
-    console.log('Loading time ' + t + ' msec');
-  }
-  phantom.exit();
-});
+
+try {
+
+	page.open(address, function(status) {
+	  if (status !== 'success') {
+    	console.log('FAIL to load the address');
+  	} else {
+    	t = Date.now() - t;
+    	console.log('Loading ' + system.args[1]);
+    	console.log('Loading time ' + t + ' msec');
+ 	 }
+
+   page.render('pictures/' + address + '.jpg', {format: 'jpeg', quality: '17'});	// JPEG screenshots
+
+  	phantom.exit();
+	});
+
+    } finally {
+
+	setTimeout(function() {
+	phantom.exit(1);
+	}, 9999);
+	      }
+}
+else
+phantom.exit();
