5b972d16370847f94923a6f11967bc59828c7811
Andreas <georationalvr@gmail.com>
2018-05-26 23:40:50 +0100

    Update generate.sh
    Loops endlessly

--- generate.sh
+++ generate.sh
@@ -1,6 +1,9 @@
+while $1; do
 while IFS=, read place ndd; do
 	prefixe="http://www."
 	url="$prefixe$ndd"
+	echo $place ;date
 	echo $url
-	./bin/phantomjs loadspeed.js $url
+	bin/phantomjs my_loadspeed_v6.js $url
 done <top-1m.csv
+done
